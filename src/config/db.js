342e1383a7c1919a4830053e6ff4df9e9a47573a
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-05-22 16:30:14 -0300

    tentatica conexão heroku pg

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,11 @@
 const { Pool } = require('pg');
 module.exports = {
   execute: async (sql, valores) => {
-    const pool = process.env.NODE_ENV == "production" ? new Pool({ssl: true}) : new Pool();
+    const pool = process.env.NODE_ENV == "production" ? new Pool({
+      ssl: {
+        rejectUnauthorized: false,
+      }
+    }) : new Pool();
     const res = await pool.query(sql, valores);
     await pool.end();
     return res.rows;
